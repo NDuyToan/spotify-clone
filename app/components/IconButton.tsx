c67451da6b27b21dcf80c573e639383739624b3a
toan_nguyen <nguyenduytoanbkdn@gmail.com>
2023-12-04 22:53:18 +0700

    side bar

---
+++ app/components/IconButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface Props {
+  icon: React.ForwardRefExoticComponent<
+    Omit<React.SVGProps<SVGSVGElement>, "ref"> & {
+      title?: string | undefined;
+      titleId?: string | undefined;
+    } & React.RefAttributes<SVGSVGElement>
+  >;
+  label: string;
+}
+
+const IconButton = ({ icon: Icon, label }: Props) => {
+  return (
+    <button className="flex items-center space-x-2 hover:text-white">
+      <Icon className="h-5 w-5" />
+      <span>{label}</span>
+    </button>
+  );
+};
+
+export default IconButton;
